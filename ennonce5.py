885801ba10414e16f8cc8540e6b1c96b3d91ac7c
CarligneCarpel <141691266+CarligneCarpel@users.noreply.github.com>
2023-08-07 23:45:26 -0400

    Add files via upload

---
+++ ennonce5.py
@@ -0,0 +1,16 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> p=5,4
+>>> type(p)
+<class 'tuple'>
+>>> g=58
+>>> type(g)
+<class 'int'>
+>>> h='2eme jour coding chalenge'
+>>> type(h)
+<class 'str'>
+>>> b=4.5
+>>> c=3.2
+>>> type(b)
+<class 'float'>
+>>> 
---
+++ enonce4.py
@@ -0,0 +1,15 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=20.5
+>>> b=40
+>>> a+b
+60.5
+>>> a-b
+-19.5
+>>> b-a
+19.5
+>>> a/b
+0.5125
+>>> a*b
+820.0
+>>> 
